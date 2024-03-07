41920107572ee09bf7ba3a78f2a34eddfefda939
suhailvs <suhailvs@gmail.com>
2024-03-07 19:56:46 +0530

    made compact

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import LocalDateTime from "@/app/components/local-time";
 interface Block {
   id: number;
   hash: string;
@@ -39,49 +40,53 @@ const HomeTable = async () => {
       <br />
       <div className="row">
         <div className="col-lg-6 offset-lg-3">
-          <div className="card">
-            <h5 className="card-header">Latest Blocks</h5>
-            <div className="card-body">
-              <div className="list-group">
-                {last_5_blocks_data.result.reverse().map((block_data) => (
-                  <div
-                    className="list-group-item list-group-item-action"
-                    key={block_data.block.id}
-                  >
-                    <div className="d-flex w-100 justify-content-between">
-                      <h4 className="mb-1">
-                        <Link className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
-                          href={`/block/${block_data.block.id}`}
-                        >
-                          #{block_data.block.id}
-                        </Link>
-                        
-                      </h4>
-                      <small>
-                        {new Date(
+          <h5>Latest Blocks</h5>
+
+          <div className="list-group">
+            {last_5_blocks_data.result.reverse().map((block_data) => (
+              <div
+                className="list-group-item list-group-item-action"
+                key={block_data.block.id}
+              >
+                <div className="d-flex w-100 justify-content-between">
+                  <h4 className="mb-1">
+                    <Link
+                      className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                      href={`/block/${block_data.block.id}`}
+                    >
+                      #{block_data.block.id}
+                    </Link>
+                  </h4>
+                  <small>
+                    {new Date(
                           block_data.block.timestamp * 1000
                         ).toLocaleString()}
-                      </small>
-                    </div>
-                    <span className={styles.text_small}>
-                      Hash: <strong>{block_data.block.hash}</strong>
-                    </span>
-                    <br />
-                    <Link
-                      href={`/address/${block_data.block.address}`}
-                      className="link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
-                    >
-                      <small>{block_data.block.address}</small>
-                    </Link>
-                    <br />
-                    <small>
-                      {block_data.transactions.length} {"Txs • "}
-                      {block_data.block.difficulty} Difficulty
-                    </small>
-                  </div>
-                ))}
+                                      </small>
+                </div>
+                <span className={styles.text_small}>
+                  Hash:{" "}
+                  <strong>
+                    {block_data.block.hash.slice(0, 6)}-
+                    {block_data.block.hash.slice(-6)}
+                  </strong>
+                </span>
+                <br />
+                <Link
+                  href={`/address/${block_data.block.address}`}
+                  className="link-info link-offset-2 link-underline-opacity-75 link-underline-opacity-100-hover"
+                >
+                  <small>
+                    Address: {block_data.block.address.slice(0, 6)}-
+                    {block_data.block.address.slice(-6)}
+                  </small>
+                </Link>
+                <br />
+                <small>
+                  {block_data.transactions.length} {"Txs • "}
+                  {block_data.block.difficulty} Difficulty
+                </small>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
